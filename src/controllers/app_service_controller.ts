5a389b1bd1b8613d4ab43ee7a3bcf4590a57b7d0
Christentimmy <timmychris09@gmail.com>
2025-09-19 22:40:01 +0300

    phone and name lookup

--- src/controllers/app_service_controller.ts
+++ src/controllers/app_service_controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
-import UserModel from "../models/user_model";
 import { reverseImageSearch } from "../services/reverse_image_service";
 import { getNumberDetails } from "../services/twilio_service";
+import {
+  searchByPhoneNumber,
+  searchByName,
+} from "../services/whitepages_service";
 import {
   getSexOffendersNearby,
   getSexOffendersByName,
@@ -98,6 +101,39 @@ export const appServiceController = {
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Failed to fetch offender" });
+    }
+  },
+
+  phoneLookup: async (req: Request, res: Response) => {
+    try {
+      if (!req.body) {
+        return res.status(400).json({ message: "Phone is required" });
+      }
+      const { phone } = req.body;
+      if (!phone) return res.status(400).json({ message: "Phone is required" });
+
+      const result = await searchByPhoneNumber(phone);
+      res.json({ success: true, data: result });
+    } catch (err) {
+      res.status(500).json({ success: false, message: "Lookup failed" });
+    }
+  },
+
+  nameLookup: async (req: Request, res: Response) => {
+    try {
+      if (!req.body) {
+        return res.status(400).json({ message: "Phone is required" });
+      }
+      const { firstName, lastName, city, state } = req.body;
+      if (!firstName || !lastName)
+        return res
+          .status(400)
+          .json({ message: "First and last name required" });
+
+      const result = await searchByName(firstName, lastName, city, state);
+      res.json({ success: true, data: result });
+    } catch (err) {
+      res.status(500).json({ success: false, message: "Lookup failed" });
     }
   },
 };

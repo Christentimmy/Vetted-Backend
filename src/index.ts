e08ddec6880f866309f1e95e945ae2f8a96e71f0
Christentimmy <timmychris09@gmail.com>
2025-09-26 03:47:48 +0300

    admin

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const port = config.port;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://vetted-admin.onrender.com"], // Add another origin here
     credentials: true,
   })
 );
